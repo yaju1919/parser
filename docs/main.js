2cf303bc0a949d304d9307d5a050e39dcb3bc5a5
yaju1919 <46996104+yaju1919@users.noreply.github.com>
2020-01-26 13:35:05 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -7,7 +7,7 @@
     function addBtn(title, func){
         return $("<button>",{text: title}).appendTo(h).click(func);
     }
-    $("<h1>",{text:"ChromeのNetworkのRequest HeadersをJSの連想配列に変換"}).appendTo(h);
+    $("<h1>",{text:"CSSをJSの連想配列に変換"}).appendTo(h);
     var input = yaju1919.addInputText(h,{
         textarea: true,
         width: "50%",
@@ -17,13 +17,14 @@
         var str = input().split('\n').map(function(v){
             var m = v.match(/: /);
             if(!m) return '';
-            var key = v.slice(0, m.index);
-            var value = v.slice(m.index + 2);
+            var key = v.slice(0, m.index).trim();
+            var value = v.slice(m.index + 2).trim();
+            if(value.slice(-1) === ';') value = value.slice(0, -1); // CSS
             return '"' + key + '": "' + value + '",';
         }).join('\n');
-        str = 'var obj = {\n' + str + '};\n';
+        str = '{\n' + str + '\n}\n';
         result = str;
-        result_elm.text(str);
+        showResult(str);
     };
     addBtn("変換", main);
     addBtn("コピー", function(){
@@ -31,4 +32,10 @@
     });
     var result;
     var result_elm = $("<div>").appendTo(h);
+    function showResult(str){
+        result_elm.empty();
+        str.split('\n').forEach(function(v){
+            $("<div>").text(v).appendTo(result_elm);
+        });
+    }
 })();
